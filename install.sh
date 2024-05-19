f9c3dfa344b84439d6f6fa1fcd8e8eb13692057c
shahrooz <40915185+shahrooz1397@users.noreply.github.com>
2024-05-19 12:07:23 +0330

    Update install.sh
    support alpine

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ cur_dir=$(pwd)
 # check root
 [[ $EUID -ne 0 ]] && echo -e "${red}Fatal error: ${plain} Please run this script with root privilege \n " && exit 1
 
+
 # Check OS and set release variable
 if [[ -f /etc/os-release ]]; then
     source /etc/os-release
@@ -21,22 +22,8 @@ else
     echo "Failed to check the system OS, please contact the author!" >&2
     exit 1
 fi
+
 echo "The OS release is: $release"
-
-arch() {
-    case "$(uname -m)" in
-    x86_64 | x64 | amd64) echo 'amd64' ;;
-    i*86 | x86) echo '386' ;;
-    armv8* | armv8 | arm64 | aarch64) echo 'arm64' ;;
-    armv7* | armv7 | arm) echo 'armv7' ;;
-    armv6* | armv6) echo 'armv6' ;;
-    armv5* | armv5) echo 'armv5' ;;
-    s390x) echo 's390x' ;;
-    *) echo -e "${green}Unsupported CPU architecture! ${plain}" && rm -f install.sh && exit 1 ;;
-    esac
-}
-
-echo "arch: $(arch)"
 
 os_version=""
 os_version=$(grep -i version_id /etc/os-release | cut -d \" -f2 | cut -d . -f1)
@@ -79,6 +66,10 @@ elif [[ "${release}" == "oracle" ]]; then
     if [[ ${os_version} -lt 8 ]]; then
         echo -e "${red} Please use Oracle Linux 8 or higher ${plain}\n" && exit 1
     fi
+elif [[ "${release}" == "alpine" ]]; then
+    if [[ ${os_version} -lt 3 ]]; then
+        echo -e "${red} Please use Alpine Linux 3 or higher ${plain}\n" && exit 1
+    fi
 else
     echo -e "${red}Your operating system is not supported by this script.${plain}\n"
     echo "Please ensure you are using one of the following supported operating systems:"
@@ -94,9 +85,11 @@ else
     echo "- Rocky Linux 9+"
     echo "- Oracle Linux 8+"
     echo "- OpenSUSE Tumbleweed"
+    echo "- Alpine Linux 3+"
     exit 1
 
 fi
+
 
 install_base() {
     case "${release}" in
